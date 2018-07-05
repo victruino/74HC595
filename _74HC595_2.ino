de5451863f7979b8854ebfd2cc8ae09e81e5cef6
victruino <40527382+victruino@users.noreply.github.com>
2018-07-05 02:11:56 -0500

    Add files via upload

---
+++ _74HC595_2.ino
@@ -0,0 +1,88 @@
+//Pin connected to ST_CP of 74HC595
+int latchPin = 7;
+//Pin connected to SH_CP of 74HC595
+int clockPin = 8;
+////Pin connected to DS of 74HC595
+int dataPin = 4;
+byte Mix[8]={B10000000,B11000000,B11100000,B11110000,B11111000,B11111100,B11111110,B11111111};
+byte Mix2[8]={B11111111,B11111110,B11111100,B11111000,B11100000,B11000000,B10000000,B00000000};
+byte Mix3[8]={B00000000,B11111111,B00000000,B11111111,B00000000,B11111111,B00000000,B11111111};
+byte Mix4[8]={B10000001,B11000011,B11100111,B00011000,B00111100,B01111110,B11111111,B10101010};
+byte Mix5[8]={B10101010,B01010101,B10101010,B01010101,B10101010,B01010101,B10101010,B01010101};
+byte Mix6[8]={B10000000,B11000000,B11100000,B11110000,B11111000,B11111100,B11111110,B11111111};
+byte Mix7[8]={B11111111,B11111110,B11111100,B11111000,B11100000,B11000000,B10000000,B00000000};
+
+void setup() {
+  
+  pinMode(latchPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);
+ 
+}
+
+void loop() {
+  //count up routine
+  for (int j = 0; j < 8; j++) {
+    //ground latchPin and hold low for as long as you are transmitting
+    digitalWrite(latchPin, LOW);
+    shiftOut(dataPin, clockPin, LSBFIRST, (Mix[j]));
+    //return the latch pin high to signal chip that it
+    //no longer needs to listen for information
+    digitalWrite(latchPin, HIGH);
+    delay(100);
+  }
+   for (int j = 0; j < 8; j++) {
+    //ground latchPin and hold low for as long as you are transmitting
+    digitalWrite(latchPin, LOW);
+    shiftOut(dataPin, clockPin, LSBFIRST, (Mix2[j]));
+    //return the latch pin high to signal chip that it
+    //no longer needs to listen for information
+    digitalWrite(latchPin, HIGH);
+    delay(100);
+  }
+   for (int j = 0; j < 8; j++) {
+    //ground latchPin and hold low for as long as you are transmitting
+    digitalWrite(latchPin, LOW);
+    shiftOut(dataPin, clockPin, LSBFIRST, (Mix3[j]));
+    //return the latch pin high to signal chip that it
+    //no longer needs to listen for information
+    digitalWrite(latchPin, HIGH);
+    delay(100);
+  }
+  for (int j = 0; j < 8; j++) {
+    //ground latchPin and hold low for as long as you are transmitting
+    digitalWrite(latchPin, LOW);
+    shiftOut(dataPin, clockPin, LSBFIRST, (Mix4[j]));
+    //return the latch pin high to signal chip that it
+    //no longer needs to listen for information
+    digitalWrite(latchPin, HIGH);
+    delay(500);
+  }
+  for (int j = 0; j < 8; j++) {
+    //ground latchPin and hold low for as long as you are transmitting
+    digitalWrite(latchPin, LOW);
+    shiftOut(dataPin, clockPin, LSBFIRST, (Mix5[j]));
+    //return the latch pin high to signal chip that it
+    //no longer needs to listen for information
+    digitalWrite(latchPin, HIGH);
+    delay(500);
+  }
+  for (int j = 0; j < 8; j++) {
+    //ground latchPin and hold low for as long as you are transmitting
+    digitalWrite(latchPin, LOW);
+    shiftOut(dataPin, clockPin, MSBFIRST, (Mix6[j]));
+    //return the latch pin high to signal chip that it
+    //no longer needs to listen for information
+    digitalWrite(latchPin, HIGH);
+    delay(100);
+  }
+  for (int j = 0; j < 8; j++) {
+    //ground latchPin and hold low for as long as you are transmitting
+    digitalWrite(latchPin, LOW);
+    shiftOut(dataPin, clockPin, MSBFIRST, (Mix7[j]));
+    //return the latch pin high to signal chip that it
+    //no longer needs to listen for information
+    digitalWrite(latchPin, HIGH);
+    delay(100);
+  }
+}
---
+++ _74HC595_3.ino
@@ -0,0 +1,29 @@
+//Pin conectado al ST_CP del 74HC595
+int latchPin = 7;
+//Pin conectado l SH_CP del 74HC595
+int clockPin = 8;
+//Pin conectado al DS del 74HC595
+
+int dataPin = 4;
+
+void setup() {
+  
+  pinMode(latchPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);
+ 
+}
+
+void loop() {
+ 
+  for (int j = 0; j < 256; j++) {
+    //primero se debe mantener el latchPin en LOW para que mantenga los datos
+    digitalWrite(latchPin, LOW);
+    //B00000000 son los 8 bits en binario, le sumaremos j para hacer un contador
+    shiftOut(dataPin, clockPin, LSBFIRST, B0000000 + j);
+    // despues de colocar los 8 bits en el puerto de salida del 74HC595
+    // se activa el latchPin en HIGH para que salgan los datos por los pines Q0-Q7
+    digitalWrite(latchPin, HIGH);
+    delay(100);
+  }
+}
